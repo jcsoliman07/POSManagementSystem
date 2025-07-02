fc8e08d16768feb78c3562d0d9292805fc9386f5
Jomar Soliman <jomarsoliman01@gmail.com>
2025-07-02 14:18:26 +0800

    Removing redundant use statements, not used

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
-use App\Models\Products;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,10 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Products;
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
