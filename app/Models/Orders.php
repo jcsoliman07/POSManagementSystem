4f2c5e1072b8722b1a2593333e7f11ce39c5d120
Jomar Soliman <jomarsoliman01@gmail.com>
2025-07-05 13:47:30 +0800

    Renaming the OrderFactory to OrdersFactory to match the Orders

--- app/Models/Orders.php
+++ app/Models/Orders.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\User;
 use App\Models\OrderItems;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -11,6 +12,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Orders extends Model
 {
     //
+    use HasFactory;
+    
     protected $fillable = ['user_id', 'total'];
 
     public function user():BelongsTo
--- database/factories/OrdersFactory.php
+++ database/factories/OrdersFactory.php
@@ -2,19 +2,17 @@
 
 namespace Database\Factories;
 
+use App\Models\Orders;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
  */
-class OrderFactory extends Factory
+class OrdersFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Orders::class;
+    
     public function definition(): array
     {
         return [
