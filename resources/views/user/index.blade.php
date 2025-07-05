e2b44e426dc8120b9b3890474a680071e0c7edc3
Jomar Soliman <jomarsoliman01@gmail.com>
2025-07-05 13:01:19 +0800

    Exchange the position of Cancel and Corfirm Order Button

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -141,15 +141,6 @@
                 </div>
 
                  <div class="mt-6 flex justify-end gap-3">
-                    <!-- Order Button Confirm-->
-                    <button 
-                        type="submit" 
-                        id="confirm-order-btn" 
-                        class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-                    > 
-                        Confirm Order 
-
-                    </button>
 
                     <!-- Order Button Cancel-->
                     <button     
@@ -158,6 +149,16 @@
                         class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                     >
                         Cancel
+
+                    </button>
+
+                    <!-- Order Button Confirm-->
+                    <button 
+                        type="submit" 
+                        id="confirm-order-btn" 
+                        class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                    > 
+                        Confirm Order 
 
                     </button>
                 </div>
