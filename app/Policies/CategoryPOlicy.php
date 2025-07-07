28c85b6038dde6c4e25cf06d126dac4e513a46eb
Jomar Soliman <jomarsoliman01@gmail.com>
2025-07-07 23:13:47 +0800

    Change namne from CategoryPOlicy to CategoryPolicy

--- app/Policies/CategoryPOlicy.php
+++ app/Policies/CategoryPOlicy.php
@@ -6,7 +6,7 @@ use App\Models\Category;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class CategoryPOlicy
+class CategoryPolicy
 {
     /**
      * Determine whether the user can view any models.
