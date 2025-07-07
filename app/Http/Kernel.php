5e6a366d732154b24afbc3bd9f0a167539e5ce12
Jomar Soliman <jomarsoliman01@gmail.com>
2025-07-07 23:42:43 +0800

    Add Super Admin and Admin auth
    Manually Added Kernel

---
+++ app/Http/Kernel.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance;
+use Illuminate\Foundation\Http\Middleware\TrimStrings;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+
+class Kernel extends HttpKernel
+{
+    /**
+     * The bootstrap classes for the application.
+     */
+    protected array $bootstrappers = [
+        \Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables::class,
+        \Illuminate\Foundation\Bootstrap\LoadConfiguration::class,
+        \Illuminate\Foundation\Bootstrap\HandleExceptions::class,
+        \Illuminate\Foundation\Bootstrap\RegisterFacades::class,
+        \Illuminate\Foundation\Bootstrap\RegisterProviders::class,
+        \Illuminate\Foundation\Bootstrap\BootProviders::class,
+    ];
+
+    /**
+     * The application's global HTTP middleware stack.
+     */
+    protected array $middleware = [
+        \Illuminate\Http\Middleware\TrustHosts::class,
+        \Illuminate\Http\Middleware\TrustProxies::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
+        PreventRequestsDuringMaintenance::class,
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        TrimStrings::class,
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+    ];
+
+    /**
+     * The application's route middleware groups.
+     */
+    protected array $middlewareGroups = [
+        'web' => [
+            EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+
+        'api' => [
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \Illuminate\Routing\Middleware\ThrottleRequests::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
+
+    /**
+     * The application's route middleware.
+     */
+    protected array $middlewareAliases = [
+        'auth' => Authenticate::class,
+        'guest' => RedirectIfAuthenticated::class,
+        'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
+        'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+    ];
+}
--- routes/web.php
+++ routes/web.php
@@ -24,22 +24,32 @@ Route::middleware('auth')->group(function () {
     Route::post('/user-dashboard', [UserController::class, 'store'])->name('orders.store');
 });
 
+//Super Admin and Admin Dashboard
+Route::middleware(['auth', 'role:admin,super_admin'])->group(function() {
+    Route::get('/dashboard', [DashBoardController::class, 'index'])->name('dashboard');
 
-Route::get('/dashboard', [DashBoardController::class, 'index'])->name('dashboard');
+    //Category
+    Route::get('/category', [CategoryController::class, 'index']);
+    Route::post('/category', [CategoryController::class, 'store']);
+    Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
 
-// Route Product
-// Route::resource('/category', [CategoryController::class, 'indexx']); //Accessing to all method
-Route::get('/category', [CategoryController::class, 'index']);
-Route::post('/category', [CategoryController::class, 'store']);
-Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
-Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+    //Product
+    Route::get('/products', [ProductsController::class, 'index']);
+    Route::post('/products', [ProductsController::class, 'store']);
+    Route::put('/products/{product}', [ProductsController::class, 'update'])->name('products.update');
 
+});
 
-// Route Product
-Route::get('/products', [ProductsController::class, 'index']);
-Route::post('/products', [ProductsController::class, 'store']);
-Route::put('/products/{product}', [ProductsController::class, 'update'])->name('products.update');
-Route::delete('/products/{product}', [ProductsController::class, 'destroy'])->name('products.destroy');
+//Super Admibn only
+Route::middleware(['auth', 'role:super_admin'])->group(function() {
+    
+    //Category
+    Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+
+    //Product
+    Route::delete('/products/{product}', [ProductsController::class, 'destroy'])->name('products.destroy');
+}); 
+
 
 
 
