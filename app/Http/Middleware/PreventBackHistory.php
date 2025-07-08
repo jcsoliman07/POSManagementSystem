6d5a075fc49b6e61218cc90f5e8e7c9e59c57201
Jomar Soliman <jomarsoliman01@gmail.com>
2025-07-08 13:24:25 +0800

    Add method for preventing cached paged after logout

--- app/Http/Middleware/PreventBackHistory.php
+++ app/Http/Middleware/PreventBackHistory.php
@@ -15,6 +15,12 @@ class PreventBackHistory
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        $response = $next($request);
+
+        $response->headers->set('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate');
+        $response->headers->set('Pragma', 'no-cache');
+        $response->headers->set('Expires', 'Sat, 01 Jan 1999 00:00:00 GMT');
+
+        return $response;
     }
 }
