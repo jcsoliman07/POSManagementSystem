6e7a9393da9c118e567ab85355633d4673fce5f0
Jomar Soliman <jomarsoliman01@gmail.com>
2025-07-08 15:55:01 +0800

    Re-optimized DashBoard Controller

--- app/Http/Controllers/DashBoardController.php
+++ app/Http/Controllers/DashBoardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Orders;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class DashBoardController extends Controller
 {
@@ -13,14 +14,21 @@ class DashBoardController extends Controller
     {
         $user = Auth::user();
 
-        $todayStats = Orders::selectRaw('COUNT(*) as order_count, SUM(total_amount) as revenue')
-                ->whereDate('created_at', today()) //Filter using date today
+        // $todayStats = Orders::selectRaw('COUNT(*) as order_count, SUM(total_amount) as revenue')
+        //         ->whereDate('created_at', today()) //Filter using date today
+        //         ->first();
+
+        $todayStats = DB::table('orders')
+                ->join('order_items', 'orders.id', '=' , 'order_items.order_id' )
+                ->selectRaw('
+                    COUNT(DISTINCT orders.id) as order_count,
+                    SUM(order.total_amount) as revenue,
+                    SUM(order_items.quantity) as total_items_sold
+                
+                ')
+                ->whereDate('created_at', today())
                 ->first();
 
-        return view('components.dashboard', [
-            'user' => $user,
-            'todayOrders' => $todayStats->order_count?? 0,
-            'todayRevenue' => number_format($todayStats->revenue ?? 0, 2),
-        ]);
+        return view('components.dashboard', compact('user', 'todayStats'));
     }
 }
