399b0fd0ee35446b9d1070b7e4badafb1a0b2d76
Jomar Soliman <jomarsoliman01@gmail.com>
2025-07-08 15:14:06 +0800

    Change the order_numer to order_number

--- app/Models/Orders.php
+++ app/Models/Orders.php
@@ -14,7 +14,7 @@ class Orders extends Model
     //
     use HasFactory;
     
-    protected $fillable = ['user_id', 'total_amount'];
+    protected $fillable = ['order_number', 'user_id', 'total_amount',];
 
     public function user():BelongsTo
     {
@@ -38,7 +38,7 @@ class Orders extends Model
             $count = static::whereYear('created_at', now()->year)->count() + 1;
 
             //Format: Order id #ORD-25 + increment numbers
-            $order->order_numer = 'ORD-' . $year . str_pad($count, 2, 0, STR_PAD_LEFT);
+            $order->order_number = 'ORD-' . $year . str_pad($count, 2, 0, STR_PAD_LEFT);
         });
     }
 }
