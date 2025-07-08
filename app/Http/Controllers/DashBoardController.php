f37ea60b3c27e7725dc47eedbeadf64413613adb
Jomar Soliman <jomarsoliman01@gmail.com>
2025-07-08 15:37:07 +0800

    Add a Single Raw query for todays order and revenue

--- app/Http/Controllers/DashBoardController.php
+++ app/Http/Controllers/DashBoardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Orders;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,6 +13,14 @@ class DashBoardController extends Controller
     {
         $user = Auth::user();
 
-        return view('components.dashboard', compact('user'));
+        $todayStats = Orders::selectRaw('COUNT(*) as order_count, SUM(total_amount) as revenue')
+                ->whereDate('created_at', today()) //Filter using date today
+                ->first();
+
+        return view('components.dashboard', [
+            'user' => $user,
+            'todayOrders' => $todayStats->order_count?? 0,
+            'todayRevenue' => $todayStats->revenue ?? 0,
+        ]);
     }
 }
