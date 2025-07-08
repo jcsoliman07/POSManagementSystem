a53a5f0fc58a8b7a80d76756d706d7add6f20303
Jomar Soliman <jomarsoliman01@gmail.com>
2025-07-08 15:19:24 +0800

    Removing Order_number

--- database/migrations/2025_06_25_154118_create_orders_table.php
+++ database/migrations/2025_06_25_154118_create_orders_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('order_number')->unique();
+            // $table->string('order_number')->unique();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->decimal('total_amount', 10, 2)->default(0.00);
             // $table->string('status')->default('pending');
