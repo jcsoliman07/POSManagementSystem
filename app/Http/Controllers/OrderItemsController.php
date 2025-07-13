3324f53ad583c688419f3f6ca7e0f778f6634806
Jomar <jomarsoliman01@gmail.com>
2025-07-13 21:58:48 +0800

    Create Order data fetching with USer, Product and Order Items

--- app/Http/Controllers/OrderItemsController.php
+++ app/Http/Controllers/OrderItemsController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\OrderItems;
+use App\Models\Orders;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OrderItemsController extends Controller
 {
     //
+    public function index()
+    {
+        $orders = Orders::with([
+                'user:id,name', //The user for each Order
+                'items.product.category:id,name' //The items for each order, the product for each item, the category for each product
+            ])
+            ->get();
+        
+        // return response()->json($orders);
+        return view('order.index', compact('orders'));
+    }
 }
