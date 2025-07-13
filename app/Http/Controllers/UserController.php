e56a29d0da8deb30f88f4b8a48ab752d7bbdf4c2
Jomar <jomarsoliman01@gmail.com>
2025-07-13 20:43:31 +0800

    Remove the function data fronm index and store, move it to OrderService Controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,11 +16,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
-        $products = Products::latest()->get()->keyBy('id');
-        $categories = Category::all();
 
-        return view('user.index', compact('products', 'categories'));
     }
 
     /**
@@ -37,54 +33,7 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //Validate Data
-        $request->validate([
-            'orderData' => 'required|json',
-        ]);
 
-        //Decoding the json data
-        $orderData = json_decode($request->orderData, true);
-
-        $totalAmount = array_reduce($orderData, function ($carry, $item){
-            return $carry + $item['subtotal'];
-        }, 0);
-
-        if(!is_array($orderData))
-        {
-            return back()->withErrors(['orderData' => 'Invalid Order Data Recieved!']);
-        }
-
-        //Create and Store Data to Order Table
-        $order = Orders::create([
-            'user_id' => Auth::id(), //Use the Authenticated Login User ID
-        ]);
-
-        $totalAmount = 0; //Preparing Total Amount of Order
-
-        //Create and Store Data for Order Items
-        foreach ($orderData as $item) {
-            
-            // dd([
-            //         'orderData' => $orderData, //Fetch data for each item
-            //         'totalAmount' =>$totalAmount, //Total Amount of price for all item
-            // ]);
-
-            OrderItems::create([
-                'order_id' => $order->id,
-                'product_id' => $item['product_id'],
-                'quantity' => $item['quantity'],
-                'price' => $item['subtotal'],
-            ]);
-
-            $totalAmount += $item['subtotal']; //Get the sum price of product in order
-
-        }
-
-        $order->update([
-            'total_amount' => $totalAmount,
-        ]);
-
-        return redirect()->back()->with('success', 'Order Successfully placed!');
     }
 
     /**
