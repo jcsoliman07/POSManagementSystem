12de778b6d1830a1bd2cd6859d7f9f06553d9b84
Jomar <jomarsoliman01@gmail.com>
2025-07-15 01:39:19 +0800

    Add codition for created format, today, yesterday, and for the rest of date

--- app/Models/Orders.php
+++ app/Models/Orders.php
@@ -25,13 +25,19 @@ class Orders extends Model
             $order->saveQuietly();
         });
     }
-
+    
     public function getFormattedCreatedAttributes()
     {
         $createdAt = $this->created_at;
 
         if ($createdAt->isToday()) {
             return 'Today, ' . $createdAt->format('h:i A');
+        }
+        elseif ($createdAt->isYesterday()){
+            return 'Yesterday, ' . $createdAt->format('h:i A');
+        }
+        else{
+            return $createdAt->format('M-d-Y, h:i A');
         }
     }
 
