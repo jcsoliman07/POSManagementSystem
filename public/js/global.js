9ba5be5305a3bacefe2d76c07f6be34c39c0a559
Jomar <jomarsoliman01@gmail.com>
2025-07-14 23:18:49 +0800

    Add another variation of custome color yellow, darker

--- public/js/global.js
+++ public/js/global.js
@@ -7,6 +7,7 @@ tailwind.config = {
                 'custom-gray': '#1f1f1f',
                 'custom-yellow': '#E9BB3F',
                 'custom-yellow-dark': '#FDD835',
+                'custom-yellow-darker': '#c28e00ff',
                 'custom-yellow-light': '#fcf2c8ff',
                 'custom-light-gray': '#F6F6F6',
                 'custom-dark-gray': '#979797',
