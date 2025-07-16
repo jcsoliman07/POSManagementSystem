3cb9f967f31933d674ab91a14611bf16f6f95e4c
Jomar <jomarsoliman01@gmail.com>
2025-07-16 22:17:16 +0800

    Access DashboardStatsService to the Order Management controller

--- app/Http/Controllers/OrderItemsController.php
+++ app/Http/Controllers/OrderItemsController.php
@@ -4,12 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\OrderItems;
 use App\Models\Orders;
+use App\Services\DashboardStatsService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class OrderItemsController extends Controller
 {
-    //
+    //Declare a protected property to hold a instance of DashboardStatsService
+    //This allows us to access the service methods through controller
+    protected $dashboardStatsService;
+
+    //This constructor automatically gives us access to the DashboardStatsService
+    //We can use the methods im this controller
+    public function __construct(DashboardStatsService $dashboardStatsService)
+    {
+        $this->dashboardStatsService = $dashboardStatsService;
+    }
+
     public function index()
     {
         $orders = Orders::with([
@@ -21,6 +32,9 @@ class OrderItemsController extends Controller
             ->paginate(10);
         
         // return response()->json($orders);
-        return view('order.index', compact('orders'));
+
+        $todayStats = $this->dashboardStatsService->getTodayStats();
+
+        return view('order.index', compact('orders', 'todayStats'));
     }
 }
