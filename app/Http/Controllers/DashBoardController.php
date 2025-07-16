c815e62311ad345b4f3213a8c57a6e19cd3ec954
Jomar <jomarsoliman01@gmail.com>
2025-07-16 15:43:59 +0800

    Add a constructor to give us automatically access the DashboardStatsService

--- app/Http/Controllers/DashBoardController.php
+++ app/Http/Controllers/DashBoardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Orders;
+use App\Services\DashboardStatsService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -12,6 +13,14 @@ class DashBoardController extends Controller
     //Declare a protected property to hold a instance of DashboardStatsService
     //This allows us to access the service methods through controller
     protected $dasboardStatsService;
+
+    //This constructor automatically gives us access to the DashboardStatsService
+    //We can use the methods im this controller
+    public function __construct(DashboardStatsService $dasboardStatsService)
+    {
+        $this->dasboardStatsService = $dasboardStatsService;
+    }
+
 
     public function index()
     {
