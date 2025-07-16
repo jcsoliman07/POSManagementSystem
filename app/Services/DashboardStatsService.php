af6a440134433f09d38e99c0b3d06fa8c65184d6
Jomar <jomarsoliman01@gmail.com>
2025-07-16 15:31:51 +0800

    Create a summarize order statistics with start and end date

--- app/Services/DashboardStatsService.php
+++ app/Services/DashboardStatsService.php
@@ -7,7 +7,19 @@ use Illuminate\Support\Carbon;
 
 class DashboardStatsService{
 
-    
+    protected function getStatsBetween($startDate, $endDate)
+    {
 
+        return DB::table('orders')
+                ->join('order_items', 'orders.id', '=' , 'order_items.order_id' )
+                ->selectRaw('
+                    COUNT(DISTINCT orders.id) as order_count,
+                    SUM(orders.total_amount) as revenue,
+                    SUM(order_items.quantity) as total_items_sold
+                
+                ')
+                ->whereDate('orders.created_at', [$startDate,$endDate])
+                ->first();
+    }
 
 }
