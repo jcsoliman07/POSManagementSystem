042e69857490e4cee59f6d9902120e7be3afe17b
Jomar <jomarsoliman01@gmail.com>
2025-07-16 21:43:59 +0800

    Split the logic for statics of todays revenue, order, and total items to avoid duplication

--- app/Services/DashboardStatsService.php
+++ app/Services/DashboardStatsService.php
@@ -2,37 +2,58 @@
 
 namespace App\Services;
 
+use Illuminate\Container\Attributes\Log;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Log as FacadesLog;
 
 class DashboardStatsService{
 
     protected function getStatsBetween($startDate, $endDate)
-    {
+    {   
+        //Split the Query to avoid Duplication
+        //Orders Count and Revenue
+        $orderStats = DB::table('orders')
+            ->whereBetween('created_at', [$startDate, $endDate])
+            ->selectRaw('COUNT(id) as order_count, SUM(total_amount) as revenue')
+            ->first();
 
-        return DB::table('orders')
-                ->join('order_items', 'orders.id', '=' , 'order_items.order_id' )
-                ->selectRaw('
-                    COUNT(DISTINCT orders.id) as order_count,
-                    SUM(orders.total_amount) as revenue,
-                    SUM(order_items.quantity) as total_items_sold
-                
-                ')
-                ->whereBetween('orders.created_at', [$startDate,$endDate])
-                ->first();
+        //Total Items Sold
+        $totalItemsSold = DB::table('order_items')
+            ->join('orders' , 'order_items.order_id', '=' , 'orders.id')
+            ->whereBetween('orders.created_at', [$startDate, $endDate])
+            ->selectRaw('SUM(order_items.quantity) as total_items_sold')
+            ->value('total_items_sold');
+        
+        return (object)[
+            'order_count'       => $orderStats->order_count ?? 0,
+            'total_amount'      => $orderStats->revenue ?? 0,
+            'total_items_sold'  => $totalItemsSold->total_items_sold ?? 0,
+        ];
     }
 
     //Get todays statistics
     public function getTodayStats()
     {
-        return $this->getStatsBetween(now()->startOfDay(),now()->endOfDay());
+        $start = now('Asia/Manila')->startOfDay();
+        $end = now('Asia/Manila')->endOfDay();
+
+        FacadesLog::info('Fetching stats between: ', ['start' => $start, 'end' => $end]);
+
+        $stats = $this -> getStatsBetween($start, $end);
+
+        FacadesLog::info('Fetch stats:', (array) $stats);
+
+        return $stats;
+            
+        // return $this->getStatsBetween($start, $end);
     }
 
-    //Get yesterdays statistics
-    public function getYesterdayStats()
-    {
-        $yesterday = now()->subDay();
-        return $this->getStatsBetween($yesterday->startOfDay(),$yesterday->endOfDay());
-    }
+    // //Get yesterdays statistics
+    // public function getYesterdayStats()
+    // {
+    //     $yesterday = now()->subDay();
+    //     return $this->getStatsBetween($yesterday->startOfDay(),$yesterday->endOfDay());
+    // }
 
 }
