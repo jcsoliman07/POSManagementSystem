797249ba26be6fbbefe67861342f12ff3befe859
Jomar <jomarsoliman01@gmail.com>
2025-07-16 13:28:15 +0800

    Remove unnecessary details in the view-order-items-modal

---
+++ resources/views/components/buttons/view-order-items-modal.blade.php
@@ -0,0 +1,9 @@
+@props(['icon'])
+
+<button
+    type="button"
+    onclick="toggleModal('view-order-items-modal{{  }}')"
+    {{ $attributes->merge(['class' => 'text-indigo-600 hover:text-indigo-900']) }}
+>
+    {{ $slot }}
+</button>
