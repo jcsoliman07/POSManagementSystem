627998f290fb50b61e80804ada6004908441a128
Jomar <jomarsoliman01@gmail.com>
2025-07-16 15:11:28 +0800

    Remove padding top 20 from view order items modal

--- resources/views/components/modal/view-order-items-modal.blade.php
+++ resources/views/components/modal/view-order-items-modal.blade.php
@@ -3,7 +3,7 @@
 
 <div id="view-order-items-modal{{ $order->id }}"
     aria-hidden="true"
-    class="fixed inset-0 z-50 flex items-start justify-center pt-20 bg-black bg-opacity-50 opacity-0 pointer-events-none transition-opacity duration-300 ease-out"
+    class="fixed inset-0 z-50 flex items-start justify-center bg-black bg-opacity-50 opacity-0 pointer-events-none transition-opacity duration-300 ease-out"
 >
     <div class="relative p-4 w-full max-w-4xl h-full md:h-auto">
         <div class="relative p-4 bg-custom-light-gray rounded-lg shadow">
