1b34894e729653ccbb6d433fb205dec8e51ed662
Jomar <jomarsoliman01@gmail.com>
2025-07-24 23:15:54 +0800

    Change the close modal function nanme

--- resources/views/components/modal/destroy-modal-category.blade.php
+++ resources/views/components/modal/destroy-modal-category.blade.php
@@ -9,7 +9,7 @@
         <div class="relative bg-white rounded-lg shadow-sm dark:bg-custom-gray">
 
             <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal"
-                onclick="closeModalDestroy('destroy-modal-category{{ $category->id }}')"> <!-- The Modal will close when it's click -->
+                onclick="closeModal('destroy-modal-category{{ $category->id }}')"> <!-- The Modal will close when it's click -->
 
                 <i class="fa-solid fa-xmark"></i>
                 <span class="sr-only">Close modal</span>
@@ -31,7 +31,7 @@
                     </button>
                     <button 
                             type="button"
-                            onclick="closeModalDestroy('destroy-modal-category{{ $category->id }}')"
+                            onclick="closeModal('destroy-modal-category{{ $category->id }}')"
                             class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                     >
                         No, cancel
