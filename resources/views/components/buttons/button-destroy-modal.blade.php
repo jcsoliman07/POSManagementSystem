dcddfe653bc4406c1707664782ee87bca2d0e00c
Jomar <jomarsoliman01@gmail.com>
2025-07-24 23:15:34 +0800

    Change the function to open modal

--- resources/views/components/buttons/button-destroy-modal.blade.php
+++ resources/views/components/buttons/button-destroy-modal.blade.php
@@ -2,7 +2,7 @@
 
 <button
     type="button"
-    onclick="openModalDestroy('destroy-modal-category{{ $category->id }}')"
+    onclick="toggleModal('destroy-modal-category{{ $category->id }}')"
     {{ $attributes->merge(['class' => 'text-red-500 hover:text-red-700']) }}
 >
     <i class="{{ $icon }}"></i>
