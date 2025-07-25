ac8c51ae69fe3007e3a57c81be0731d778c58d93
Jomar <jomarsoliman01@gmail.com>
2025-07-25 13:27:08 +0800

    Add a function onclick for reset filter

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -107,7 +107,7 @@
 
                         <!-- Filter & Reset Buttons -->
                         <div class="flex items-end gap-3">
-                            <button title="Reset Filter"
+                            <button title="Reset Filter" onclick="filterOrderData('clear')"
                                 class="flex items-center justify-center w-10 h-10 rounded-xl bg-yellow-100 text-yellow-500 hover:bg-yellow-200 transition">
                                 <i class="fa-solid fa-arrow-rotate-left"></i>
                             </button>
