79a5585eae4ce064acc7a0cfb89a1ebc4c776dba
Jomar <jomarsoliman01@gmail.com>
2025-08-01 23:54:16 +0800

    Add files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,7 @@
 import './bootstrap';
 import './global';
 import '../css/app.css';
+import '../../public/js/admin';
+import '../../public/js/user';
+import '../../public/js/global';
+import '../../public/css/global';
