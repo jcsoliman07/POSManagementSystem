4dccdd6d8d346f3550245343bd7f008bae6e6216
Jomar <jomarsoliman01@gmail.com>
2025-08-09 10:34:25 +0800

    Create and add RotueServiceProvider

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Products;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+        Route::model('product', Products::class);
+    }
+}
