6a72d3637b5e0511a7c0553be9ed33e9c51bef48
Jomar <jomarsoliman01@gmail.com>
2025-08-11 12:52:35 +0800

    Add animations for category

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -2,30 +2,32 @@
 
     <x-wrapper>
 
-        <x-forms.heading>Category Management</x-forms.heading>
-        <x-forms.paragraph class="text-md mb-6 border-b pb-2">Manage your Categories here.</x-forms.paragraph>
+        <main class="category-container">
+            <x-forms.heading>Category Management</x-forms.heading>
+            <x-forms.paragraph class="text-md mb-6 border-b pb-2">Manage your Categories here.</x-forms.paragraph>
 
-            <div class="mb-4">
-                <x-buttons.button-add-modal target="add-new-category-modal">New</x-buttons.button-add-modal>
-                <x-modal.add-new-category-modal />
-            </div>
-
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                @forelse ($categories as $category)
-                    <x-new-card :$category />
-                @empty
-            </div>
-                <div class="flex justify-center items-center h-full">
-                    <x-forms.paragraph> No Category yet </x-forms.paragraph>
+                <div class="mb-4">
+                    <x-buttons.button-add-modal target="add-new-category-modal">New</x-buttons.button-add-modal>
+                    <x-modal.add-new-category-modal />
                 </div>
-                @endforelse
 
-            {{-- <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                @foreach ($categories as $category)
-                    <x-new-card :$category />
-                @endforeach 
-            </div>           --}}
-        </div>
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 category-card">
+                    @forelse ($categories as $category)
+                        <x-new-card :$category />
+                    @empty
+                </div>
+                    <div class="flex justify-center items-center h-full">
+                        <x-forms.paragraph> No Category yet </x-forms.paragraph>
+                    </div>
+                    @endforelse
+
+                {{-- <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                    @foreach ($categories as $category)
+                        <x-new-card :$category />
+                    @endforeach 
+                </div>           --}}
+            </div>
+        </main>
         
     </x-wrapper>
     
